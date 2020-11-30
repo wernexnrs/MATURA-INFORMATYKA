d2fe90658ed86d6f6d6c74d753127e11c6c1c069
wernexnrs <wernexnrs@gmail.com>
2020-11-30 18:17:42 +0100

    Create  prime_factors.py

---
+++ Python/Algorytmy/prime_factors.py
@@ -0,0 +1,9 @@
+def prime_factors(n):
+    factors = []
+    k = 2
+    while n != 1:
+        while n % k == 0:
+            n //= k
+            factors.append(k)
+        k += 1
+    return factors
