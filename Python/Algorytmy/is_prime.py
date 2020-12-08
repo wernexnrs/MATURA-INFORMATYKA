a69e17e797dbff2b2a7d8f4b6a09eb8bb82e204d
wernexnrs <wernexnrs@gmail.com>
2020-12-08 20:37:07 +0100

    Update is_prime.py

--- Python/Algorytmy/is_prime.py
+++ Python/Algorytmy/is_prime.py
@@ -1,8 +1,7 @@
-def is_prime(n):
-    if n < 2:
-        return False
-    else:
-        for i in range(2,n):
-            if n % i == 0:
+def is_prime(x):
+    if x > 1:
+        for i in range(2,x):
+            if (x % i) == 0:
                 return False
-    return True
+        return True
+    else: return False
