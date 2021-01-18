ab31bf6ce47ecc1de26e4f5476d7ed86dd7db06b
wernexnrs <wernexnrs@gmail.com>
2021-01-18 17:28:30 +0100

    Update Number_Conversion.py

--- Python/Algorytmy/Number_Conversion.py
+++ Python/Algorytmy/Number_Conversion.py
@@ -1,18 +1,7 @@
-str = '1000'
+dec_numbers = [15,12,2,4,7,8]
 
-print(int(str,2)) # decimal -> binary  
-print(int(str,8)) # decimal -> octal  
-print(int(str,16)) # decimal -> hexadecimal 
+dec_TO_bin = [bin(i)[2:] for i in dec_numbers]
+dec_TO_hex = [hex(i)[2:] for i in dec_numbers]
 
-binary = '1000' # 8 - dec
-
-print(int(binary,2)) # binary -> decimal
-
-
-octal = '0100' # 64 - dec
-
-print(int(octal,8)) # octal -> decimal
-
-hex = 'FFFF' # 65535 - dec
-
-print(int(hex,16)) # hexadecimal -> decimal
+bin_TO_dec = [int(i,2) for i in dec_TO_bin]
+hex_TO_dec = [int(i,16) for i in dec_TO_hex]
