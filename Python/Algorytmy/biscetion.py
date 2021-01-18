90df29cd5fa073a837795fba078fa3f8e4f97093
wernexnrs <wernexnrs@gmail.com>
2021-01-18 21:26:04 +0100

    Create biscetion.py

---
+++ Python/Algorytmy/biscetion.py
@@ -0,0 +1,18 @@
+def f(x):
+    return -4*x +2
+
+def bisection(a,b,precyzja = 0.0001): 
+    if f(a)*f(b) >= 0: 
+        return None
+    c = a 
+    while (b-a) >= precyzja: 
+        c = (a+b)/2
+        if (f(c) == 0.0): 
+            break
+        if (f(c)*f(a) < 0): 
+            b = c 
+        else: 
+            a = c      
+    return c
+      
+print("{:.3f}".format( bisection(-100, 100) ))
