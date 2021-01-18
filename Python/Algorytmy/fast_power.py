579ab478d503a788516e025ccef9ac88f6a381a4
wernexnrs <wernexnrs@gmail.com>
2021-01-18 20:54:34 +0100

    Create fast_power.py

---
+++ Python/Algorytmy/fast_power.py
@@ -0,0 +1,19 @@
+def fast_power(x,n):
+    if n == 0:
+        return 1
+    elif n % 2 != 0:
+        return x * x**(n-1)
+    else:
+        a = x**(n/2)
+    return a**2
+
+def fast_power2(x,n):
+    if n == 0:
+        return 1
+    elif n % 2 != 0:
+        return x * (x**((n-1)/2))**2
+    return (x**(n/2))**2
+
+
+print(fast_power(3,6))
+print(fast_power2(3,6))
