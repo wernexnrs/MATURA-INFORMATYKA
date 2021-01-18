9ed1d4c480407e17b63c2ecc1637ca7ad7f81794
wernexnrs <wernexnrs@gmail.com>
2021-01-18 17:38:45 +0100

    Update NWW-NWD.py

--- Python/Algorytmy/NWW-NWD.py
+++ Python/Algorytmy/NWW-NWD.py
@@ -1,11 +1,4 @@
-def nwd(a, b): return nwd(b, a%b) if b else a
-
-#^the same
-def nwd(a, b):
-    if b > 0:
-        return nwd(b, a%b)
-    else:
-        return a
+def nwd(a, b): return nwd(b, a%b) if b else a #recursive
 
 #^the same
 def nwd(a, b):
@@ -13,16 +6,8 @@ def nwd(a, b):
         return nwd(b, a%b)
     return a
 
-#iteracyjnie
+#iteration
 def nwd_i(a, b):
     while b:
         a, b = b, a%b
-    return a
-
-#iteracyjnie ze zmienna pomocnicza
-def nwd_i(a, b):
-    while b:
-        temp = a
-        a = b
-        b = temp%b
     return a
