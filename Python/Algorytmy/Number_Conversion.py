59c45288c847d129edb75d783a6d8b1fd24a63f9
wernexnrs <wernexnrs@gmail.com>
2021-01-18 20:44:42 +0100

    Update Number_Conversion.py

--- Python/Algorytmy/Number_Conversion.py
+++ Python/Algorytmy/Number_Conversion.py
@@ -1,7 +1,25 @@
 dec_numbers = [15,12,2,4,7,8]
 
 dec_TO_bin = [bin(i)[2:] for i in dec_numbers]
+dec_TO_oct = [oct(i)[2:] for i in dec_numbers]
 dec_TO_hex = [hex(i)[2:] for i in dec_numbers]
 
+
 bin_TO_dec = [int(i,2) for i in dec_TO_bin]
+oct_TO_dec = [int(i,8) for i in dec_TO_oct]
 hex_TO_dec = [int(i,16) for i in dec_TO_hex]
+
+
+BS="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+def to_base(n, b): 
+    return BS[n] if n < b else to_base(n // b, b) + BS[n % b]
+
+def to_base2(s, b):
+    res = ""
+    while s:
+        res+=BS[s%b]
+        s//= b
+    return res[::-1] or "0"
+
+print(to_base(12, 2))
+print(to_base2(12, 2))
